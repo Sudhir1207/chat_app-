bd11162490b6f8b72ce2f7025915793cb824cb67
sudhirr@karunya.edu.in <sudhirr@karunya.edu.in>
2025-03-14 09:59:34 +0530

    initial setup & created user model

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+
+const PORT = process.env.PORT;
+connectDB();
+app.use("/api/auth", authRoutes);
+app.listen(PORT, () => {
+  console.log("server is running on  " + PORT);
+});
