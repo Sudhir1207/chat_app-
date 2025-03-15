65b391d8d0ef29c35454c3d5d8baaba668a66e8d
sudhirr@karunya.edu.in <sudhirr@karunya.edu.in>
2025-03-15 14:30:11 +0530

    profile update access restriction

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 const PORT = process.env.PORT;
 app.use(express.json());
+app.use(cookieParser());
 connectDB();
 app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
