771469bcc61deecf66d7266b6c3990300ef0cf3c
sudhirr@karunya.edu.in <sudhirr@karunya.edu.in>
2025-03-16 19:47:04 +0530

    Message model & Controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 connectDB();
 app.use("/api/auth", authRoutes);
+app.use("api/message", messageRoutes);
 app.listen(PORT, () => {
   console.log("server is running on  " + PORT);
 });
