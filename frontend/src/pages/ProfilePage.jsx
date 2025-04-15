cbb124d293abad2cad7f0eae849965cf8b92b2b6
sudhirr@karunya.edu.in <sudhirr@karunya.edu.in>
2025-04-15 22:42:40 +0530

    started working on profile page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { ImagePlus } from "lucide-react";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center gap-y-3">
+      <h1>Profile</h1>
+      <span>My Info</span>
+      <div className="w-[15%] h-auto relative">
+        <img
+          src="/cat.jpg"
+          alt=""
+          className="rounded-full border-4 border-slate-200 "
+        />
+        <div className="absolute  top-[100px] left-[90px] -translate-y-1/2 border-3 border-black">
+          <ImagePlus className="w-5 h-5 p-1 bg-white text-black rounded-full" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
