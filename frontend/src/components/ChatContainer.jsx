2c0d9ff421825dfdd6b671b7a5aed7db1a49e106
sudhirr@karunya.edu.in <sudhirr@karunya.edu.in>
2025-04-26 00:01:33 +0530

    completed chatContainer (UI + Functionality)

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -3,16 +3,17 @@ import { useChatStore } from "../store/useChatStore";
 import { useEffect } from "react";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
-import { Divide } from "lucide-react";
 import MessagesSkeleton from "./skeletons/MessagesSkeleton";
-
+import { useAuthStore } from "../store/useAuthStore";
 const ChatContainer = () => {
   const { messages, getMessages, isMessagesLoading, selectedUser } =
     useChatStore();
 
-  // useEffect(() => {
-  //   getMessages(selectedUser._id);
-  // }, [selectedUser._id, getMessages]);
+  const { authUser } = useAuthStore();
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
 
   if (isMessagesLoading)
     return (
@@ -26,7 +27,46 @@ const ChatContainer = () => {
   return (
     <div className="flex flex-col flex-1 overflow-auto">
       <ChatHeader />
-      <p>Messages...</p>
+      <div className="w-full p-3 pt-1 pb-1 h-[65vh] flex flex-col bg-slate-800">
+        <div className="overflow-y-auto p-4 space-y-4 scrollbar-none flex-1">
+          {messages.map((message) => (
+            <div
+              key={message._id}
+              className={`chat ${
+                message.senderId === authUser._id ? "chat-end" : "chat-start"
+              }`}
+            >
+              <div className="chat-image avatar">
+                <div className="size-10 rounded-full border">
+                  <img
+                    src={
+                      message.senderId === authUser._id
+                        ? authUser.profilePic
+                        : "/circle-user.svg"
+                    }
+                    alt="profile"
+                  />
+                </div>
+              </div>
+              <div className="chat-header mb-1">
+                <time className="text-xs opacity-50 ml-1">
+                  {message.createdAt}
+                </time>
+              </div>
+              <div className="chat-bubble flex">
+                {message.image && (
+                  <img
+                    src={message.image}
+                    alt="attachment"
+                    className="sm:max-w-[120px] rounded-lg mb-2"
+                  />
+                )}
+                {message.text && <p>{message.text}</p>}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
       <MessageInput />
     </div>
   );
--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,7 +1,121 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { useChatStore } from "../store/useChatStore";
+import { Image, Send, X } from "lucide-react";
+import toast from "react-hot-toast";
 
 const MessageInput = () => {
-  return <div>MessageInput</div>;
+  const [text, setText] = useState("");
+  const [imgPrv, setImgPrv] = useState(null);
+  const inputRef = useRef(null);
+  const { sendMessage } = useChatStore();
+
+  const handleImgChange = (e) => {
+    const file = e.target.files[0];
+    if (!file.type.startsWith("image/")) {
+      toast.error("Select an image file");
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImgPrv(reader.result);
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  const removeImage = () => {
+    setImgPrv(null);
+
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  };
+
+  const handleSndMsg = async (e) => {
+    e.preventDefault();
+    console.log("Clicked send button");
+    if (!text.trim() && !imgPrv) return;
+
+    try {
+      console.log("Sending to backend:", {
+        text: text.trim(),
+        image: imgPrv,
+      });
+
+      await sendMessage({
+        text: text.trim(),
+        image: imgPrv,
+      });
+
+      setText("");
+      setImgPrv(null);
+      if (inputRef.current) {
+        inputRef.current.value = "";
+      }
+    } catch (error) {
+      console.error("Failed to send message", error);
+    }
+  };
+
+  return (
+    <div className="w-full relative">
+      <div className="absolute -translate-y-24 translate-x-2 -left-4 right-0 px-2">
+        {imgPrv && (
+          <div className="relative bg-slate-900 p-2 w-full">
+            <img
+              src={imgPrv || "/vj.jpg"}
+              alt="prev"
+              className="w-20 h-20 object-cover rounded-lg  "
+            />
+            <button
+              onClick={removeImage}
+              className="absolute -translate-y-[78px] translate-x-[68px] hover:text-orange-600 cursor-pointer"
+            >
+              <X className="size-3" />
+            </button>
+          </div>
+        )}
+      </div>
+
+      <form
+        onSubmit={handleSndMsg}
+        className="flex items-center gap-1 bg-slate-900 p-3"
+      >
+        <div className="flex-1 flex gap-1">
+          <input
+            type="text"
+            value={text}
+            className="w-full input input-bordered rounded-lg input-sm lg:input-md focus:outline-none "
+            placeholder="write a message"
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input
+            type="file"
+            className="hidden"
+            ref={inputRef}
+            onChange={handleImgChange}
+          />
+          <button
+            type="button"
+            className={`hidden sm:flex btn btn-sm lg:btn-md btn-circle border-2 ${
+              imgPrv ? "text-emerald-500" : "text-zinc-400"
+            }`}
+            onClick={() => inputRef.current?.click()}
+          >
+            <Image className="size-4 rounded-full" />
+          </button>
+        </div>
+        <button
+          className="btn btn-circle btn-sm hover:btn-accent"
+          disabled={!text.trim() && !imgPrv}
+          type="submit"
+        >
+          <Send className="size-4 text-orange-600" />
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default MessageInput;
