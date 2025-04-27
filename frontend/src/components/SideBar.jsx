da476c4dd77450bb3af0f08a75fa5c921bad76a1
sudhirr@karunya.edu.in <sudhirr@karunya.edu.in>
2025-04-27 23:31:00 +0530

    showing online users in UI

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,17 +1,23 @@
 import { Users } from "lucide-react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useChatStore } from "../store/useChatStore";
 import SidebarSkeleton from "./skeletons/SidebarSkeleton";
+import { useAuthStore } from "../store/useAuthStore";
 
 const SideBar = () => {
   const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } =
     useChatStore();
 
-  const onlineUsers = [];
+  const { onlineUsers } = useAuthStore();
+  const [showOnlineUsers, setShowOnlineUsers] = useState(false);
 
   useEffect(() => {
     getUsers();
   }, [getUsers]);
+
+  const filteredUsers = showOnlineUsers
+    ? users.filter((user) => onlineUsers.includes(user._id))
+    : users;
 
   if (isUsersLoading) return <SidebarSkeleton />;
   return (
@@ -26,10 +32,15 @@ const SideBar = () => {
           <input
             type="checkbox"
             name="showOnline"
+            checked={showOnlineUsers}
+            onChange={(e) => setShowOnlineOnly(e.target.checked)}
             className="appearance-none w-3 h-3 border border-gray-300 rounded-md checked:bg-green-600 checked:border-transparent focus:outline-none transition duration-300"
           />
-          <span className="ml-2 text-[10px] lg:text-md text-nowrap">
+          <span className="ml-2 text-[15px] lg:text-md text-nowrap">
             Show online only
+          </span>
+          <span className="text-xs ml-2 text-gray-500">
+            ({onlineUsers.length - 1} online)
           </span>
         </label>
       </div>
@@ -37,7 +48,7 @@ const SideBar = () => {
       {/* <div className="border-t-[1px] border-orange-600"></div> */}
 
       <div className="overflow-y-auto w-full scrollbar-thumb-orange-600 scrollbar-thin scrollbar-track-slate-950">
-        {users.map((user) => (
+        {filteredUsers.map((user) => (
           <button
             key={user._id}
             onClick={() => setSelectedUser(user)}
@@ -55,7 +66,9 @@ const SideBar = () => {
                   !user.profilePic ? "bg-white" : ""
                 }`}
               />
-              <span className="absolute size-2 -translate-y-9 translate-x-2 bg-green-500 rounded-full ring-2 ring-black"></span>
+              {onlineUsers.includes(user._id) && (
+                <span className="absolute size-2 -translate-y-9 translate-x-2 bg-green-500 rounded-full ring-2 ring-black" />
+              )}
             </div>
             <div className="hidden lg:flex flex-col items-start ml-2 ">
               <div className="ml-2">{user.fullName}</div>
