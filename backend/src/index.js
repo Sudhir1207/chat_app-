66b568224af99437423aa3e03a90cd8626b24272
Sudhir_120702 <sudhirr@karunya.edu.in>
2025-12-02 22:45:16 +0530

    processing client origin from env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,30 +1,30 @@
-import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
-import { connectDB } from "./lib/db.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
+import { app, server } from './lib/socket.js'
+import { connectDB } from './lib/db.js'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
+    credentials: true
   })
-);
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use(cookieParser());
+)
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ limit: '10mb', extended: true }))
+app.use(cookieParser())
 
-connectDB();
+connectDB()
 
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+app.use('/api/auth', authRoutes)
+app.use('/api/messages', messageRoutes)
 
 server.listen(PORT, () => {
-  console.log("server is running on " + PORT);
-});
+  console.log('server is running on ' + PORT)
+})
